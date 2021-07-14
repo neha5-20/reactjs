3176bcefdf08025af10c955465e7fc82b374e392
Neha <86823550+neha5-20@users.noreply.github.com>
2021-07-14 13:02:45 +0530

    Add files via upload

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Button.css"
+
+const Button = ({buttonText}) => {
+    return(
+        <button className="btn">{buttonText}</button>
+    )
+}
+
+export default Button
